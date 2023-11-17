9a0f0430303b18bc6346b3760e98f8347ca19cd2
TungPT12 <mylifeundermycontrol@gmail.com>
2023-11-17 15:44:59 +0700

    create index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+const app = express();
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true
+}))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Running on http://localhost:${process.env.PORT}`)
+})
