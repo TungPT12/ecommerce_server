b9278ecc9b86e4f766a30d8305bde209ce365e45
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-05 19:17:10 +0700

    create model order

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,7 +3,50 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
+    delivery: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    items: [
+        {
+            product: {
+                type: Schema.Types.ObjectId,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        }
+    ]
 })
 
 
