b51769aa590dc2bb279ec94c47bc72ce8c991d85
TungPT12 <mylifeundermycontrol@gmail.com>
2023-12-06 17:43:54 +0700

    css email letter

--- src/controllers/client/order.js
+++ src/controllers/client/order.js
@@ -1,5 +1,6 @@
 const Order = require("../../models/Order");
 const User = require("../../models/User");
+const formatPrice = require("../../utils/FormatPrice");
 const getKeyEnvironmentVariable = require("../../utils/getKeyEnvironmentVariable");
 const sendEmail = require("../../utils/send-email");
 const winston = require("../../utils/winston");
@@ -13,22 +14,24 @@ exports.createOrder = async (req, res) => {
         let itemsForOrder = [];
 
         items.forEach((item) => {
-
-            itemsForEmail.push(`<div>
-                ${item.product.name}
-            </div>
-            <div>
-                <img src="${item.product.images[0].includes("http") ? item.product.images[0] : (getKeyEnvironmentVariable('BASE_URL') + "/" + item.product.images[0])}" alt="asd"/>
-            </div>
-            <div>
-                ${item.product.price}
-            </div>
-            <div>
-                ${item.quantity}
-            </div>
-            <div>
-                ${(item.quantity * item.product.price)}
-            </div>
+            itemsForEmail.push(`
+               <div id="row" style="display: flex;font-size: 1.2rem;">
+                    <div style="text-align: center;width:30%;border:1px solid #fff;color:#fff;margin: 0.6px;overflow: hidden;word-break: break-word;padding:4px;">
+                        ${item.product.name}
+                    </div>
+                    <div style="text-align: center;width:10%;border: 1px solid #fff;color: #fff;margin:0.6px;padding: 7px 8px 4px 8px;">
+                        <img style="width: 100%" src="${item.product.images[0].includes("http") ? item.product.images[0] : (getKeyEnvironmentVariable('BASE_URL') + "/" + item.product.images[0])}" alt="asd"/>
+                    </div>
+                    <div style="text-align: center;width:20%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:4px;">
+                        ${formatPrice((item.product.price).toString())} VND
+                    </div>
+                    <div style="text-align: center;width:10%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:4px;">
+                        ${item.quantity}
+                    </div>
+                    <div style="text-align: center;width:20%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:4px;">
+                        ${formatPrice((item.quantity * item.product.price).toString())} VND
+                    </div>
+               </div>
             `);
             itemsForOrder.push({
                 product: item.product._id,
@@ -36,26 +39,42 @@ exports.createOrder = async (req, res) => {
                 quantity: item.quantity,
             })
         })
-        console.log(itemsForEmail[0])
-        const html = `<div>
-            <h2>Xin chào ${name}</h2>
-           <div>
-                <h4>Phone: ${phone}</h4>
-                <h4>Address: ${address}</h4>
-           </div>
-           <div>
-                <div>
-                    <div id="header-row">
-                        <div>Tên Sản Phẩm</div>
-                        <div>Hình Ảnh</div>
-                        <div>Giá</div>
-                        <div>Số Lượng</div>
-                        <div>Thành Tiền</div>
+        const html = ` 
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta name="viewport" content="width=device-width">
+                    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                </head>
+                <body>
+                    <div style="background-color:#000;color:#fff;padding:3rem;font-family: 'Roboto';">
+                        <h1>Xin chào: ${name}</h1>
+                        <div style="font-size: 1rem;margin: 2.5rem 0;"">
+                            <h4 style="margin-bottom: 0.5rem;">Phone: ${phone}</h4>
+                            <h4 style="margin: 0 0;">Address: ${address}</h4>
+                        </div>
+                        <div>
+                            <div>
+                                <div id="header-row" style="display: flex;font-size: 1rem">
+                                    <div style="text-align: center;width:30%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:14px 4px;">Tên Sản Phẩm</div>
+                                    <div style="text-align: center;width:10%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:14px 8px;">Hình Ảnh</div>
+                                    <div style="text-align: center;width:20%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:14px 4px;">Giá</div>
+                                    <div style="text-align: center;width:10%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:14px 4px;">Số Lượng</div>
+                                    <div style="text-align: center;width:20%;border: 1px solid #fff;color: #fff;margin:0.6px;overflow: hidden;word-break: break-word;padding:14px 4px;">Thành Tiền</div>
+                                </div>
+                            </div>
+                            <div>
+                                ${itemsForEmail.join('')}
+                            </div>
+                        </div>
+                        <div style="color: #fff;margin:2rem 0rem;">
+                            <h1 style="color: #fff;margin: 0;">Tổng Thanh Toán</h1>
+                            h1 style="color: #fff;margin: 0;">${formatPrice(totalPrice.toString())} VND</h1>
+                        </div>
+                        <h1 style="color: #fff;">Cảm ơn bạn !</h1>
                     </div>
-                </div>
-                <div>${itemsForEmail.join('')}</div>
-           </div>
-        </div> `
+                </body>
+            </html> `
 
         const order = new Order({
             user: userId,
